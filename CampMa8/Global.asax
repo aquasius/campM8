098508f084e2302faa84aca8a15a44f2308c898b
Aaron Quasius <aquasius92@gmail.com>
2020-01-28 12:45:04 -0600

    initia commit

---
+++ CampMa8/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CampMa8.MvcApplication" Language="C#" %>
---
+++ CampMa8/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CampMa8.Startup))]
+namespace CampMa8
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
