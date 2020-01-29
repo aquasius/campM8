25fbfc41eaafa0f482d6a58172789f734c1d088b
Aaron Quasius <aquasius92@gmail.com>
2020-01-29 08:17:03 -0600

    created my google places key model

---
+++ CampMa8/Models/GooglePlacesKey.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CampMa8.Models
+{
+    public class GooglePlacesKey
+    {
+        public static string Key = "AIzaSyAlrMGDp5h0bH8FlUpHwFIVjHp9ZN8TSxg";
+    }
+}
